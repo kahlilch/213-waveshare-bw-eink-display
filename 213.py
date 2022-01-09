e53b10fe0774bf065c0c98cdbc20ae3c3d1a0a4a
kahlilch <97400023+kahlilch@users.noreply.github.com>
2022-01-09 16:32:54 +0000

    Create 213.py

---
+++ 213.py
@@ -0,0 +1,451 @@
+# Uses a modified version of the Waveshare team micropython drivers, notice below -
+# *****************************************************************************
+# * | File        :	  Pico_ePaper-2.13.py
+# * | Author      :   Waveshare team
+# * | Function    :   Electronic paper driver
+# * | Info        :
+# *----------------
+# * | This version:   V1.0
+# * | Date        :   2021-03-16
+# # | Info        :   python demo
+# -----------------------------------------------------------------------------
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documnetation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to  whom the Software is
+# furished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+
+from machine import Pin, SPI
+import framebuf
+import utime
+
+lut_full_update= [
+    0x80,0x60,0x40,0x00,0x00,0x00,0x00,             #LUT0: BB:     VS 0 ~7
+    0x10,0x60,0x20,0x00,0x00,0x00,0x00,             #LUT1: BW:     VS 0 ~7
+    0x80,0x60,0x40,0x00,0x00,0x00,0x00,             #LUT2: WB:     VS 0 ~7
+    0x10,0x60,0x20,0x00,0x00,0x00,0x00,             #LUT3: WW:     VS 0 ~7
+    0x00,0x00,0x00,0x00,0x00,0x00,0x00,             #LUT4: VCOM:   VS 0 ~7
+
+    0x03,0x03,0x00,0x00,0x02,                       # TP0 A~D RP0
+    0x09,0x09,0x00,0x00,0x02,                       # TP1 A~D RP1
+    0x03,0x03,0x00,0x00,0x02,                       # TP2 A~D RP2
+    0x00,0x00,0x00,0x00,0x00,                       # TP3 A~D RP3
+    0x00,0x00,0x00,0x00,0x00,                       # TP4 A~D RP4
+    0x00,0x00,0x00,0x00,0x00,                       # TP5 A~D RP5
+    0x00,0x00,0x00,0x00,0x00,                       # TP6 A~D RP6
+
+    0x15,0x41,0xA8,0x32,0x30,0x0A,
+]
+
+lut_partial_update = [ #20 bytes
+    0x00,0x00,0x00,0x00,0x00,0x00,0x00,             #LUT0: BB:     VS 0 ~7
+    0x80,0x00,0x00,0x00,0x00,0x00,0x00,             #LUT1: BW:     VS 0 ~7
+    0x40,0x00,0x00,0x00,0x00,0x00,0x00,             #LUT2: WB:     VS 0 ~7
+    0x00,0x00,0x00,0x00,0x00,0x00,0x00,             #LUT3: WW:     VS 0 ~7
+    0x00,0x00,0x00,0x00,0x00,0x00,0x00,             #LUT4: VCOM:   VS 0 ~7
+
+    0x0A,0x00,0x00,0x00,0x00,                       # TP0 A~D RP0
+    0x00,0x00,0x00,0x00,0x00,                       # TP1 A~D RP1
+    0x00,0x00,0x00,0x00,0x00,                       # TP2 A~D RP2
+    0x00,0x00,0x00,0x00,0x00,                       # TP3 A~D RP3
+    0x00,0x00,0x00,0x00,0x00,                       # TP4 A~D RP4
+    0x00,0x00,0x00,0x00,0x00,                       # TP5 A~D RP5
+    0x00,0x00,0x00,0x00,0x00,                       # TP6 A~D RP6
+
+    0x15,0x41,0xA8,0x32,0x30,0x0A,
+]
+
+EPD_WIDTH       = 250 # 122
+EPD_HEIGHT      = 128
+
+RST_PIN         = 12
+DC_PIN          = 8
+CS_PIN          = 9
+BUSY_PIN        = 13
+
+FULL_UPDATE = 0
+PART_UPDATE = 1
+
+class EPD_2in13(framebuf.FrameBuffer):
+    def __init__(self):
+        self.reset_pin = Pin(RST_PIN, Pin.OUT)
+        
+        self.busy_pin = Pin(BUSY_PIN, Pin.IN, Pin.PULL_UP)
+        self.cs_pin = Pin(CS_PIN, Pin.OUT)
+        self.width = EPD_WIDTH
+        self.height = EPD_HEIGHT
+        
+        self.full_lut = lut_full_update
+        self.partial_lut = lut_partial_update
+        
+        self.full_update = FULL_UPDATE
+        self.part_update = PART_UPDATE
+        
+        self.spi = SPI(1)
+        self.spi.init(baudrate=4000_000)
+        self.dc_pin = Pin(DC_PIN, Pin.OUT)
+        
+        
+        self.buffer = bytearray(self.width * self.height // 8)
+        super().__init__(self.buffer, self.width, self.height, framebuf.MONO_HLSB)
+        self.init(FULL_UPDATE) 
+
+    def rotate_image(self):
+        new_buffer = bytearray(self.width * self.height // 8)
+        for i in range(self.width * self.height // 8):
+            new_buffer[i] = buffer[1 + width * i]
+        buffer = new_buffer
+
+    def digital_write(self, pin, value):
+        pin.value(value)
+
+    def digital_read(self, pin):
+        return pin.value()
+
+    def delay_ms(self, delaytime):
+        utime.sleep(delaytime / 1000.0)
+
+    def spi_writebyte(self, data):
+        self.spi.write(bytearray(data))
+
+    def module_exit(self):
+        self.digital_write(self.reset_pin, 0)
+
+    # Hardware reset
+    def reset(self):
+        self.digital_write(self.reset_pin, 1)
+        self.delay_ms(50)
+        self.digital_write(self.reset_pin, 0)
+        self.delay_ms(2)
+        self.digital_write(self.reset_pin, 1)
+        self.delay_ms(50)   
+
+
+    def send_command(self, command):
+        self.digital_write(self.dc_pin, 0)
+        self.digital_write(self.cs_pin, 0)
+        self.spi_writebyte([command])
+        self.digital_write(self.cs_pin, 1)
+
+    def send_data(self, data):
+        self.digital_write(self.dc_pin, 1)
+        self.digital_write(self.cs_pin, 0)
+        self.spi_writebyte([data])
+        self.digital_write(self.cs_pin, 1)
+        
+    def ReadBusy(self):
+        while(self.digital_read(self.busy_pin) == 1):      # 0: idle, 1: busy
+            self.delay_ms(8)
+        
+    def TurnOnDisplay(self):
+        self.send_command(0x22)
+        self.send_data(0xC7)
+        self.send_command(0x20)        
+        self.ReadBusy()
+
+    def TurnOnDisplayPart(self):
+        self.send_command(0x22)
+        self.send_data(0x0c)
+        self.send_command(0x20)        
+        self.ReadBusy()
+
+    def init(self, update):
+        print('init')
+        self.reset()
+        if(update == self.full_update):
+            self.ReadBusy()
+            self.send_command(0x12) # soft reset
+            self.ReadBusy()
+
+            self.send_command(0x74) #set analog block control
+            self.send_data(0x54)
+            self.send_command(0x7E) #set digital block control
+            self.send_data(0x3B)
+
+            self.send_command(0x01) #Driver output control
+            self.send_data(0x27)
+            self.send_data(0x01)
+            self.send_data(0x01)
+            
+            self.send_command(0x11) #data entry mode
+            self.send_data(0x01)
+
+            self.send_command(0x44) #set Ram-X address start/end position
+            self.send_data(0x00)
+            self.send_data(0x0F)    #0x0C-->(15+1)*8=128
+
+            self.send_command(0x45) #set Ram-Y address start/end position
+            self.send_data(0x27)   #0xF9-->(249+1)=250
+            self.send_data(0x01)
+            self.send_data(0x2e)
+            self.send_data(0x00)
+            
+            self.send_command(0x3C) #BorderWavefrom
+            self.send_data(0x03)
+
+            self.send_command(0x2C)     #VCOM Voltage
+            self.send_data(0x55)    #
+
+            self.send_command(0x03)
+            self.send_data(self.full_lut[70])
+
+            self.send_command(0x04) #
+            self.send_data(self.full_lut[71])
+            self.send_data(self.full_lut[72])
+            self.send_data(self.full_lut[73])
+
+            self.send_command(0x3A)     #Dummy Line
+            self.send_data(self.full_lut[74])
+            self.send_command(0x3B)     #Gate time
+            self.send_data(self.full_lut[75])
+
+            self.send_command(0x32)
+            for count in range(70):
+                self.send_data(self.full_lut[count])
+
+            self.send_command(0x4E)   # set RAM x address count to 0
+            self.send_data(0x00)
+            self.send_command(0x4F)   # set RAM y address count to 0X127
+            self.send_data(0x0)
+            self.send_data(0x00)
+            self.ReadBusy()
+        else:
+            self.send_command(0x2C)     #VCOM Voltage
+            self.send_data(0x26)
+
+            self.ReadBusy()
+
+            self.send_command(0x32)
+            for count in range(70):
+                self.send_data(self.partial_lut[count])
+
+            self.send_command(0x37)
+            self.send_data(0x00)
+            self.send_data(0x00)
+            self.send_data(0x00)
+            self.send_data(0x00)
+            self.send_data(0x40)
+            self.send_data(0x00)
+            self.send_data(0x00)
+
+            self.send_command(0x22)
+            self.send_data(0xC0)
+            self.send_command(0x20)
+            self.ReadBusy()
+
+            self.send_command(0x3C) #BorderWavefrom
+            self.send_data(0x01)
+        return 0       
+        
+    def display(self, image):
+        self.send_command(0x24)
+        for j in range(0, self.width):
+            for i in range(0, int(self.height / 8)):
+                self.send_data(image[i + j * int(self.height / 8)])   
+        self.TurnOnDisplay()
+        
+    def displayPartial(self, image):
+        self.send_command(0x24)
+        for j in range(0, self.width):
+            for i in range(0, int(self.height / 8)):
+                self.send_data(image[i + j * int(self.height / 8)])   
+                
+        self.send_command(0x26)
+        for j in range(0, self.width):
+            for i in range(0, int(self.height / 8)):
+                self.send_data(~image[i + j * int(self.height / 8)])  
+        self.TurnOnDisplayPart()
+
+    def displayPartBaseImage(self, image):
+        self.send_command(0x24)
+        for j in range(0, self.width):
+            for i in range(0, int(self.height / 8)):
+                self.send_data(image[i + j * int(self.height / 8)])   
+                
+        self.send_command(0x26)
+        for j in range(0, self.width):
+            for i in range(0, int(self.height / 8)):
+                self.send_data(image[i + j * int(self.height / 8)])  
+        self.TurnOnDisplay()
+    
+    def Clear(self, color=0xff):
+        self.send_command(0x24)
+        for j in range(0, self.width):
+            for i in range(0, int(self.height / 8)):
+                self.send_data(color)
+        self.send_command(0x26)
+        for j in range(0, self.width):
+            for i in range(0, int(self.height / 8)):
+                self.send_data(color)
+                                
+        self.TurnOnDisplay()
+
+    def sleep(self):
+        self.send_command(0x10) #enter deep sleep
+        self.send_data(0x03)
+        self.delay_ms(2000)
+        self.module_exit()
+
+    def mytext(self, text):
+        pass
+
+    def set_bit(self, v, index, x):
+        """Set the index:th bit of v to 1 if x is truthy, else to 0, and return the new value."""
+        mask = 1 << index   # Compute mask, an integer with just bit 'index' set.
+        v &= ~mask          # Clear the bit indicated by the mask (if x is False)
+        if x:
+            v |= mask         # If x was True, set the bit indicated by the mask.
+        return v            # Return the result, we're done.
+
+    def bufpixel(self, x, y, c = 0, wraparound = True):
+        if wraparound:
+            x = x % EPD_WIDTH
+            y = y % EPD_HEIGHT
+        else:
+            if x > EPD_WIDTH: return
+            if y > EPD_HEIGHT: return
+        bit_index = ((EPD_WIDTH - (1 + x)) * EPD_HEIGHT) + y
+        buffer_index = bit_index // 8
+        bitno = 7 - bit_index % 8
+        epd.buffer[buffer_index] = self.set_bit(epd.buffer[buffer_index], bitno, c)
+
+
+#ASCII Character Set
+cmap = ['00000000000000000000000000000000000', #Space
+        '00100001000010000100001000000000100', #!
+        '01010010100000000000000000000000000', #"
+        '01010010101101100000110110101001010', ##
+        '00100011111000001110000011111000100', #$
+        '11001110010001000100010001001110011', #%
+        '01000101001010001000101011001001101', #&
+        '10000100001000000000000000000000000', #'
+        '00100010001000010000100000100000100', #(
+        '00100000100000100001000010001000100', #)
+        '00000001001010101110101010010000000', #*
+        '00000001000010011111001000010000000', #+
+        '000000000000000000000000000000110000100010000', #,
+        '00000000000000011111000000000000000', #-
+        '00000000000000000000000001100011000', #.
+        '00001000010001000100010001000010000', #/
+        '01110100011000110101100011000101110', #0
+        '00100011000010000100001000010001110', #1
+        '01110100010000101110100001000011111', #2
+        '01110100010000101110000011000101110', #3
+        '00010001100101011111000100001000010', #4
+        '11111100001111000001000011000101110', #5
+        '01110100001000011110100011000101110', #6
+        '11111000010001000100010001000010000', #7
+        '01110100011000101110100011000101110', #8
+        '01110100011000101111000010000101110', #9
+        '00000011000110000000011000110000000', #:
+        '01100011000000001100011000010001000', #;
+        '00010001000100010000010000010000010', #<
+        '00000000001111100000111110000000000', #=
+        '01000001000001000001000100010001000', #>
+        '01100100100001000100001000000000100', #?
+        '01110100010000101101101011010101110', #@
+        '00100010101000110001111111000110001', #A
+        '11110010010100111110010010100111110', #B
+        '01110100011000010000100001000101110', #C
+        '11110010010100101001010010100111110', #D
+        '11111100001000011100100001000011111', #E
+        '11111100001000011100100001000010000', #F
+        '01110100011000010111100011000101110', #G
+        '10001100011000111111100011000110001', #H
+        '01110001000010000100001000010001110', #I
+        '00111000100001000010000101001001100', #J
+        '10001100101010011000101001001010001', #K
+        '10000100001000010000100001000011111', #L
+        '10001110111010110101100011000110001', #M
+        '10001110011010110011100011000110001', #N
+        '01110100011000110001100011000101110', #O
+        '11110100011000111110100001000010000', #P
+        '01110100011000110001101011001001101', #Q
+        '11110100011000111110101001001010001', #R
+        '01110100011000001110000011000101110', #S
+        '11111001000010000100001000010000100', #T
+        '10001100011000110001100011000101110', #U
+        '10001100011000101010010100010000100', #V
+        '10001100011000110101101011101110001', #W
+        '10001100010101000100010101000110001', #X
+        '10001100010101000100001000010000100', #Y
+        '11111000010001000100010001000011111', #Z
+        '01110010000100001000010000100001110', #[
+        '10000100000100000100000100000100001', #\
+        '00111000010000100001000010000100111', #]
+        '00100010101000100000000000000000000', #^
+        '00000000000000000000000000000011111', #_
+        '11000110001000001000000000000000000', #`
+        '00000000000111000001011111000101110', #a
+        '10000100001011011001100011100110110', #b
+        '00000000000011101000010000100000111', #c
+        '00001000010110110011100011001101101', #d
+        '00000000000111010001111111000001110', #e
+        '00110010010100011110010000100001000', #f
+        '000000000001110100011000110001011110000101110', #g
+        '10000100001011011001100011000110001', #h
+        '00100000000110000100001000010001110', #i
+        '0001000000001100001000010000101001001100', #j
+        '10000100001001010100110001010010010', #k
+        '01100001000010000100001000010001110', #l
+        '00000000001101010101101011010110101', #m
+        '00000000001011011001100011000110001', #n
+        '00000000000111010001100011000101110', #o
+        '000000000001110100011000110001111101000010000', #p
+        '000000000001110100011000110001011110000100001', #q
+        '00000000001011011001100001000010000', #r
+        '00000000000111110000011100000111110', #s
+        '00100001000111100100001000010000111', #t
+        '00000000001000110001100011001101101', #u
+        '00000000001000110001100010101000100', #v
+        '00000000001000110001101011010101010', #w
+        '00000000001000101010001000101010001', #x
+        '000000000010001100011000110001011110000101110', #y
+        '00000000001111100010001000100011111', #z
+        '00010001000010001000001000010000010', #{
+        '00100001000010000000001000010000100', #|
+        '01000001000010000010001000010001000', #}
+        '01000101010001000000000000000000000' #}~
+]
+
+def printchar(letter,xpos,ypos,size,charupdate):
+    origin = xpos
+    charval = ord(letter)
+    index = charval - 32
+    character = cmap[index]
+    rows = [character[i:i+5] for i in range(0,len(character),5)]
+    for row in rows:
+        for bit in row:
+            if bit == '1':
+                epd.bufpixel(xpos,ypos)
+                if size==2:
+                    epd.bufpixel(xpos+1,ypos+1)
+                    epd.bufpixel(xpos+1,ypos)
+                    epd.bufpixel(xpos,ypos+1)
+            xpos+=size
+        xpos=origin
+        ypos+=size
+    if charupdate == True:
+        epd.display(epd.buffer)
+
+def printstring(string,xpos,ypos,size,charupdate,strupdate):
+    if size == 2:
+        spacing = 14
+    else:
+        spacing = 8
+    for i in string:
+        printchar(i,xpos,ypos,size,charupdate)
+        xpos+=spacing
+    if strupdate == True:
+        epd.display(epd.buffer)
+
+if __name__=='__main__':
+    epd = EPD_2in13()
+    epd.Clear(0xff)    
+    epd.fill(0xff)
+    printstring("Hello World!", 10, 10, 2, False, False)
+    epd.display(epd.buffer)
